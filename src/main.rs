ed9a151b7b0b4383c178c14bbf2905fdd08cde60
Max Cunningham <maxzinedine@gmail.com>
2025-07-20 10:13:52 +0100

    seperate out library and add some simple cli parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,56 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Subcommand, Debug)]
+enum Command {
+    /// Add a new item.
+    Add {
+        /// The task description.
+        task: String,
+    },
+    /// Remove a specific task.
+    Remove {
+        /// The index of the task.
+        index: Vec<usize>,
+    },
+    /// Mark a task as done.
+    Done {
+        /// The index of the task.
+        index: Vec<usize>,
+    },
+    /// List all tasks.
+    List,
+    /// Clear all done tasks.
+    Clear,
+}
+
+#[derive(Parser)]
+#[command(name = "todo")]
+#[command(about = "A todo CLI", version)]
+struct Cli {
+    #[command(subcommand)]
+    command: Command,
+}
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Command::Add { task } => {
+            println!("Adding task {}", task);
+        }
+        Command::Remove { index } => {
+            println!("Removing {:?}th task", index);
+        }
+        Command::Done { index } => {
+            println!("Marking {:?}th task as complete", index);
+        }
+        Command::List => {
+            println!("Printing List");
+        }
+        Command::Clear => {
+            println!("Clearing completed tasks");
+        }
+        _ => {
+            println!("I hate you");
+        }
+    }
 }
